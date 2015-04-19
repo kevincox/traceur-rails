e33a595a67c311a8c587f6583efc2be6e6e758ca
Kevin Cox <kevincox@kevincox.ca>
2015-04-19 12:32:40 -0400

    Don't set a module style if set to nil.

--- lib/traceur/template.rb
+++ lib/traceur/template.rb
@@ -23,13 +23,22 @@ module Traceur
 
     private
 
+    def generate_config(scope)
+      c = {
+        filename: "#{file}",
+      }
+      c[:modules] = Traceur::Config.compile_to if Traceur::Config.compile_to
+
+      ::JSON.generate(c)
+    end
+
     def generate_source(scope)
       <<-SOURCE
         var traceur = require("#{traceur_path}");
-        var result  = traceur.compile(#{::JSON.generate(data, quirks_mode: true)}, {
-          filename: '#{file}',
-          modules:  '#{Traceur::Config.compile_to}'
-        });
+        var result  = traceur.compile(
+          #{::JSON.generate(data, quirks_mode: true)},
+          #{generate_config(scope)}
+        );
 
         if (result.errors && result.errors.length) {
           throw "\\n" + result.errors.join("\\n");
